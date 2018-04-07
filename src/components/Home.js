a73f41b70179f6e27be35c90118b85bcaf2b70f0
devin <deivnobowen@gmail.com>
2018-04-07 11:31:44 -0600

    boilerplate

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import "../css/home.css"
+
+
+export default class Home extends Component {
+
+  render() {
+    return (
+      <div className="home">
+        home
+      </div>
+    );
+  }
+}
